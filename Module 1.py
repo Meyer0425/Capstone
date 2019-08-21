0f2819e5d1809d44b87ada273b29406dd338c592
Meyer0425 <44820059+Meyer0425@users.noreply.github.com>
2019-08-21 15:43:29 -0500

    Add files via upload

---
+++ Module 1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#This is a notebook for the first module of the capstone course for IBM
+
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
